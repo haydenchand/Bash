573e882414a1e5c75f2c0784260b255291180016
Hayden <hmchandler@gmail.com>
2025-01-29 16:23:17 -0500

    further scripting progress

--- backupdir_adv.sh
+++ backupdir_adv.sh
@@ -6,9 +6,39 @@
 # 0 = hourly
 # 1 = daily
 # 2 = monthly
-#
+
 #!/bin/bash
 
-# Getting system date and time for archive
+# Getting system date and time for backup
 
-archive_time = $(date + "%Y-%m-%d %H:%M-%S")
+backup_time=$(date +%Y-%m-%d_%H-%M-%S)
+
+# Creating backup file name
+backup_name=backup$backup_time.tar.gz
+
+# Getting location of config file
+config_loc=/backup/backup_config.txt
+
+# Checking if config file exists
+while [ ! $config_loc ];
+do
+	echo "config file not found at: $config_loc"
+	echo "please enter a new config file location: "
+	read config_loc
+done
+echo "config found"
+# Create a list of files to be backed up by reading from conifg
+
+line_number=1
+
+while IFS= read -r file_name
+do
+	echo "$file_name"
+	if [ ! $file_name ]
+	then 
+		echo "WARNING: $file_name does not exist"
+		echo "double check line $line_number of config file"
+	fi
+	backup_list="$backup_list $file_name"
+done
+echo "done test"
